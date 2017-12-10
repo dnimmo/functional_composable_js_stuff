84d05bbe904a4cf8e5bfdfce45cbd353521603c8
David Nimmo <dnimmo@gmail.com>
2017-12-10 18:40:08 +0000

    added fromNullable example

---
+++ 3_code_branching_with_either.test.js
@@ -0,0 +1,62 @@
+const { fromNullable } = require('./utils');
+
+
+const findColour =
+  name =>
+    fromNullable({
+      red: '#ff444',
+      blue: '#3b5998',
+      yellow: '#fff68f',
+    }[name]);
+
+
+describe('findColour', () => {
+  it(
+    'should be able to return a hex code for a recorded colour',
+    () =>
+      expect(findColour('yellow')
+        .fold(
+          () => {},
+          x => x,
+        ))
+        .toBe('#fff68f'),
+  );
+
+  it(
+    'should return null if no hex code is found',
+    () =>
+      expect(findColour('chorizo')
+        .fold(
+          x => x,
+          () => {},
+        ))
+        .toBe(null),
+  );
+
+  it(
+    'should allow chained "maps" if a hex code is found',
+    () =>
+      expect(findColour('yellow')
+        .map(x => x.slice(1))
+        .map(x => x.toUpperCase())
+        .fold(
+          () => {},
+          x => x,
+        ))
+        .toBe('FFF68F'),
+  );
+
+  it(
+    'should ignore chained "maps" if a hex code is not found',
+    () =>
+      expect(findColour('chorizo')
+        .map(x => x.slice(1))
+        .map(x => x.toUpperCase())
+        .fold(
+          () => 'no colour found',
+          () => {},
+        ))
+        .toBe('no colour found'),
+  );
+});
+
