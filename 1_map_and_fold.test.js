625a7a35bad9547a1bae5be940e708fda376b948
David Nimmo <dnimmo@gmail.com>
2017-12-10 18:39:08 +0000

    added map/fold example

---
+++ 1_map_and_fold.test.js
@@ -0,0 +1,18 @@
+const { Box } = require('./utils');
+
+const nextCharForNumberString =
+  str =>
+    Box(str.trim())
+      .map(trimmed => Number(trimmed))
+      .map(number => number + 1)
+      .fold(nextNumber => String.fromCharCode(nextNumber));
+
+describe('nextCharFromNumberString', () => {
+  it(
+    'should return the next character from a string containing a number',
+    () =>
+      expect(nextCharForNumberString(' 64 '))
+        .toBe('A'),
+  );
+});
+
