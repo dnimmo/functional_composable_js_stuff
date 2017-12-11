2d38b018ee4a0de1839fdf18317bd9ae4b8262ca
David Nimmo <dnimmo@gmail.com>
2017-12-11 21:19:36 +0000

    added lazy evaluation example test

---
+++ 7_delayed_evaluation.test.js
@@ -0,0 +1,35 @@
+const { LazyBox } = require('./utils');
+
+describe('delaying evaluation with LazyBox', () => {
+  it(
+    'should not trigger an evaluation before `fold`',
+    () => {
+      const result =
+        LazyBox(() => ' 64 ')
+          .map(str => str.trim())
+          .map(trimmed => Number(trimmed))
+          .map(number => number + 1)
+          .map(x => String.fromCharCode(x));
+
+      expect(result)
+        .not.toBe('a');
+    },
+  );
+
+  it(
+    'should be able to map a number of values and return a result',
+    () => {
+      const result =
+        LazyBox(() => ' 64 ')
+          .map(str => str.trim())
+          .map(trimmed => Number(trimmed))
+          .map(number => number + 1)
+          .map(x => String.fromCharCode(x))
+          .fold(x => x.toLowerCase());
+
+      expect(result)
+        .toBe('a');
+    },
+  );
+});
+
