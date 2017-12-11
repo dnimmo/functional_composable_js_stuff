2d122220826360f85887864ac2070afacc372717
David Nimmo <dnimmo@gmail.com>
2017-12-11 00:11:16 +0000

    readability update

--- 4_nested_either_for_composable_error_handling.test.js
+++ 4_nested_either_for_composable_error_handling.test.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
-const { tryCatch } = require('./utils');
+const {
+  tryCatch,
+  returnValue,
+} = require('./utils');
+
+const returnDefaultPort =
+  () => 3000;
 
 const getPort =
   (configFileLocation = 'config.json') =>
     tryCatch(() => fs.readFileSync(configFileLocation))
       .chain(x => tryCatch(() => JSON.parse(x)))
+      .chain(({ port }) => tryCatch(() => port))
       .fold(
-        () => 3000,
-        ({ port }) => port,
+        returnDefaultPort,
+        returnValue,
       );
 
 describe('', () => {
@@ -15,16 +22,14 @@ describe('', () => {
     'should return the port from a given config file',
     () =>
       expect(getPort('config.json'))
-        .toBe(8888)
-    ,
+        .toBe(8888),
   );
 
   it(
     'should return a default port if the config file requested does not exist',
     () =>
       expect(getPort('chorizos.json'))
-        .toBe(3000)
-    ,
+        .toBe(3000),
   );
 
   it(
